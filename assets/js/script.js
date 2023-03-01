db03ea2ec923ef659ae97a9a7ab7442065017da8
jjanczynska <joaxjan@gmail.com>
2023-03-01 19:31:50 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,21 @@ document.addEventListener("DOMContentLoaded", function() {
              } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
-
-
-             }
+            }
         })
 
     }
 })
 
-
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after user's answer has been processes
+ */
 function runGame() {
 
+    //Creates ywo random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
