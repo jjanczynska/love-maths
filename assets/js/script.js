e0554ab35b73bb746778bbd97e721f23f5efccff
jjanczynska <joaxjan@gmail.com>
2023-03-02 14:14:49 +0000

    Add new html

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
 
     runGame("addition");
 
-})
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -50,8 +50,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,12 +79,21 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and icrements it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.gerElementById("incorrect").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and icrements it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.gerElementById("score").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
